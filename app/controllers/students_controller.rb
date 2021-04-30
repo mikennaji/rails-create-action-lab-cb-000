506d37a153f17c5ad0c1648b82bd1e482785be8f
Michael Nnaji <mnnaji3@uwo.ca>
2021-04-30 11:54:34 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,7 +12,10 @@ class StudentsController < ApplicationController
   end
 
   def create
-    @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
+    @student = Student.new
+    @student.first_name = params[:first_name]
+    @student.last_name =  params[:last_name]
+    @student.save
     redirect_to student_path(@student)
   end
 
